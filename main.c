8f206c4e27c5557c9dc2a4fa15536b9b7561b7f5
Vule028KM028 <55806749+Vule028KM028@users.noreply.github.com>
2023-11-09 22:14:36 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,28 @@
 //
 //  Created by Vukasin Lazarevic on 29.10.23..
 //
+
+
+/* 2. (7p) Koristeći naredbu while i funkciju getchar() za čitanje karaktera obraditi ulaznu
+sekvencu koja se sastoji od cifara, razmaka i zagrada. Izračunati zbir brojeva koji se nalaze
+unutar sekvence, pri čemu se od brojeva u malim zagradama na zbir dodaje samo vrednost
+najmanjeg parnog broja, od brojeva u velikim zagradama vrednost razlike najvećeg i
+najmanjeg broja, dok se brojevi van zagrada normalno dodaju na zbir. Zagrade ne mogu biti
+ugnježdene.
+Primer: Ulaz: {32 53 12} (31 6 2) 123 {355} (3 7)
+Izlaz: 166
+Objašnjenje:
+{32 53 12} – brojevi su u velikim zagradama pa se na zbir dodaje razlika najvećeg i najmanjeg
+broja, tj. 53-12=41
+(31 6 2) – brojevi su unutar malih zagrada, na zbir se dodaje vrednost najmanjeg parnog
+broja, tj. na zbir se dodaje 2
+123 – broj je van zagrade, na zbir se dodaje 123
+{366} – broj je unutar velikih zagrada, razlika najmanjeg i najvećeg je 366-366=0, te se na zbir
+dodaje 0
+(3 7) – brojevi su unutar malih zagrada, ali se u njima ne nalazi nijedan parni broj pa vrednost
+zbira ostaje nepromenjena
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
